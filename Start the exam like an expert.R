aee75493615fbd2aed488521dc791faddcbd8643
max mayr <info@maxmayr.com>
2020-01-30 01:21:31 +0100

    Apply the name of the Exam File

--- Start the exam like an expert.R
+++ Start the exam like an expert.R
@@ -1,5 +1,5 @@
 library(openxlsx)
-data = read.xlsx(xlsxFile = './Data.xlsx') #add file here or rename your exam file to Data.xlsx and copy it into this folder
+data = read.xlsx(xlsxFile = './STWI Exam HS19.xlsx') #add file here or rename your exam file to Data.xlsx and copy it into this folder
 attach(data) 
 # => this sign is for copy pase : ~
 # use percent => mean(stockData$`%.Fall`)
@@ -9,6 +9,8 @@ attach(data)
 #d) french = discret
 #e) siblings = discret
 #f) present = continous
+# plz = discret nominal
 library(MASS) #for fractions(amountOfSeven/(6*6)) => zeigt brüchte an
 library(Hmisc) 
 describe(c(0,1),type=2)
+describe(c(0,1))
